5db336851a415a1bad7e2c4c96927e3107990ec7
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-10 21:02:15 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+function startFan () {
+ console.log('Joss')
+}
+
+startFan();
+
+function addNumber(num1,num2){
+ sum=num1+num2;
+ console.log(sum);
+}
+
+addNumber(78,90);
---
+++ recap.js
@@ -0,0 +1,7 @@
+var arr=[11,22,33,44,55];
+var arr1=arr.slice(0,3);
+console.log(arr1);
+
+var name='Íshti';
+var name1=name.slice(0,3);
+console.log(name1)
