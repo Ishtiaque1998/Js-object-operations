bed75e29c6fab8585c5f006d26fffbc7a29f6b2c
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-12 14:35:37 +0600

    first commit

---
+++ return.js
@@ -0,0 +1,35 @@
+function add(num1, num2){
+ console.log(num1,num2);
+ var sum=num1+num2;
+ // return sum;
+ console.log(sum)
+ return sum;
+ // return 13;
+ // return 'done'
+}
+
+// add(4,5);
+
+var total=add(4,67);
+console.log('total',total);
+
+
+
+function bringBooks(money){
+ var price=10;
+ var quantity=money/price;
+ return quantity
+}
+
+var total= bringBooks(300);
+console.log(total);
+
+function kobiNaam (age){
+ // var age=90;
+ console.log('The name is Ishtiaque',age)
+ // console.log(age);
+ return age ;
+}
+
+var product=kobiNaam(67);
+console.log(product)
