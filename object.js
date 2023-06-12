43ab6caeddfcef35b57c6516abcf75ee8c3a77a2
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-12 15:36:14 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,24 @@
+const students={id:115,
+ name:'Talha', 
+ class:8
+}
+
+var mobile={
+ name:'Nokia',
+ price: 18000,
+ storage:'64 gb',
+ camera:'20 mp'
+}
+
+console.log(mobile.storage);
+console.log(students);
+
+var myComputer={
+ brand:'Lenovo',
+ price: 45000,
+ ram:'4 gb',
+ storage:'100 gb'
+}
+
+myComputer.ram='20 gb'
+console.log(myComputer.ram);
