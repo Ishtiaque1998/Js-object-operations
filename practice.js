3a19ec3888155f29053aa0cb346e6d73b3c8d2b2
Ishtiaque1997 <ishtiaque07031997@gmail.com>
2023-06-12 17:06:13 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,42 @@
+const color= 'purple';
+if (color==='green'){
+ console.log('all good');
+}
+else if(color=='blue'){
+ console.log('blueeeee')
+}
+else if(color=='red'){
+ console.log('reddddd')
+}
+else if(color=='white'){
+ console.log('whitrrrrr')
+}
+else if(color=='violet'){
+ console.log('violettttttttt')
+}
+else{
+ console.log('bsdfhhfhj')
+}
+
+
+//switch
+switch(color){
+ case 'green':
+  console.log('green');
+  break;
+ case 'blue':
+  console.log('blue');
+  break;
+ case 'blue':
+  console.log('blue');
+  break;
+ case 'violet':
+  console.log('violet');
+  break;
+ case 'yellow':
+  console.log('yellow');
+  break;
+ default:
+  console.log('all good')
+
+}
